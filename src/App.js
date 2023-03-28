661a8288500646e626a33ef02c170134d8a27033
mulukimwangeka <mwangekaruth@gmail.com>
2023-03-28 17:01:19 +0300

    routes added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Navbar from './Navbar/navbar';
+import Action from './Action/action';
+import Adventure from './Adventure/adventure';
+import Home from './Home/home';
+import Main from './Main/main';
+import mygames from './Mygames/mygames';
+import popular from './Popular/popular';
+import puzzle from './Puzzle/puzzle';
+import Race from './Race/Race';
+import Strategy from './Strategy/strategy'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar/>
+      <Route path="/Action" exact={true} component={Action} />
+      <Route path="/Adventures" exact={true} component={Adventure} />
+      <Route path="/Home" exact={true} component={Home} />
+      <Route path="/Main" exact={true} component={Main} />
+      <Route path="/mygames" exact={true} component={mygames} />
+      <Route path="/Popular" exact={true} component={popular} />
+      <Route path="/puzzle" exact={true} component={puzzle} />
+      <Route path="/Race" exact={true} component={Race} />
+      <Route path="/Strategy" exact={true} component={Strategy} />
+
+
+
+      
     </div>
+ 
   );
 }
 
---
+++ src/Navbar/navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
+function navbar() {
+}
+export default navbar;
